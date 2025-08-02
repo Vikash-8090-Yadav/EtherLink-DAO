d246c83f68d91b842c5f26a10604f9b39196d699
Vikash <yadav621gzp@gmail.com>
2025-08-03 00:48:21 +0530

    feat: update branding from Linea Club to EtherLink DAO
    - Changed sidebar branding from 'Linea Club' to 'EtherLink DAO'
    - Updated currency labels from ETH to XTZ
    - Consistent branding across all club-related pages

--- Frontend/src/pages/club.jsx
+++ Frontend/src/pages/club.jsx
@@ -373,7 +373,7 @@ function Club() {
           <div className="sidebar-brand-icon rotate-n-15">
             <i className="fas fa-laugh-wink" />
           </div>
-          <div className="sidebar-brand-text mx-3">Linea Club</div>
+                          <div className="sidebar-brand-text mx-3">EtherLink DAO</div>
         </a>
         {/* Divider */}
         <hr className="sidebar-divider my-0" />
